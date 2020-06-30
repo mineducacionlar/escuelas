36a1d0dfaa8347fb2f0de540301dbf3e93f22a3b
mineducacionlar <aaleksandroff@gmail.com>
2020-06-30 14:35:15 -0300

    make it better

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
-// app.use(express.static('../dist/adminpro'));
-
-// app.get('/*', function(req, res) {
-//     res.sendFile('index.html', {root: 'dist/adminpro/'}
-//   );
-//   });
-
-//   app.listen(process.env.PORT || 8080);
+//Install express server
+const express = require('express');
+const path = require('path');
+ 
+const app = express();
+ 
+// Serve only the static files form the dist directory
+// Replace the '/dist/<to_your_project_name>'
+app.use(express.static(__dirname + '/dist/adminpro'));
+ 
+app.get('*', function(req,res) {
+  // Replace the '/dist/<to_your_project_name>/index.html'
+  res.sendFile(path.join(__dirname + '/dist/adminpro/index.html'));
+});
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
